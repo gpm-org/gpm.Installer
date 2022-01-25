2ae7034a2dde813186eb16a7a98e325242d75d73
rfuzzo <r.fuzzo@gmail.com>
2022-01-25 11:36:09 +0100

    add installer files

---
+++ source/gpm.Installer.WPF/Constants.cs
@@ -0,0 +1,13 @@
+namespace gpm.Installer.WPF;
+
+internal class Constants
+{
+    public class Commands
+    {
+        public const string Restart = @"/Restart";
+        //public const string Dir = @"/Dir";
+        public const string Slot = @"/Slot";
+        public const string Package = @"/Package";
+    }
+
+}
---
+++ source/gpm.Installer.WPF/MainViewModel.cs
@@ -0,0 +1,87 @@
+using System;
+using System.Collections.ObjectModel;
+using System.Reactive.Linq;
+using System.Windows;
+using System.Windows.Input;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.DependencyInjection;
+using CommunityToolkit.Mvvm.Input;
+using DynamicData;
+using gpm.Core.Services;
+
+namespace gpm.Installer.WPF;
+
+public class MainViewModel : ObservableRecipient
+{
+    private readonly MySink _sink = Ioc.Default.GetRequiredService<MySink>();
+    private readonly IProgressService<double> _progressService = Ioc.Default.GetRequiredService<IProgressService<double>>();
+    private readonly MainController _mainController = Ioc.Default.GetRequiredService<MainController>();
+
+    private readonly ReadOnlyObservableCollection<string> _list;
+
+    public MainViewModel()
+    {
+        CloseCommand = new RelayCommand(Close);
+        CancelCommand = new RelayCommand(Cancel);
+
+        IsNotFinished = true;
+        _text = "";
+
+        var myOperation = _sink.Connect()
+            .Transform(x => x.RenderMessage())
+            .Bind(out _list)
+            .DisposeMany()
+            .Subscribe(x =>
+            {
+                foreach (var add in x)
+                {
+                    var msg = add.Item.Current;
+                    Text += $"{msg}\n";
+                }
+            });
+
+        _progressService.ProgressChanged += _progressService_ProgressChanged;
+        _mainController.PropertyChanged += _mainController_PropertyChanged;
+    }
+
+    private void _mainController_PropertyChanged(object? sender, System.ComponentModel.PropertyChangedEventArgs e)
+    {
+        switch (e.PropertyName)
+        {
+            case "IsFinished":
+                IsNotFinished = false;
+                IsFinished = true;
+                break;
+            default:
+                break;
+        }
+    }
+
+    private bool _isNotFinished;
+    public bool IsNotFinished { get => _isNotFinished; set => SetProperty(ref _isNotFinished, value); }
+
+    private bool _isFinished;
+    public bool IsFinished { get => _isFinished; set => SetProperty(ref _isFinished, value); }
+
+
+
+    //private bool CanCancel() => !_mainController.IsFinished;
+    private void Cancel() => Application.Current.Shutdown();
+
+    //private bool CanClose() => _mainController.IsFinished;
+    private void Close() => Application.Current.Shutdown();
+
+    public ICommand CloseCommand { get; }
+    public ICommand CancelCommand { get; }
+
+    private void _progressService_ProgressChanged(object? sender, double e) => ProgressValue = e * 100;
+
+    private string _text;
+    public string Text { get => _text; set => SetProperty(ref _text, value); }
+
+
+    private double _progressValue;
+    public double ProgressValue { get => _progressValue; set => SetProperty(ref _progressValue, value); }
+
+
+}
